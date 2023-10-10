20f40d89207598933e1de0ebc5fdb2ead784d562
OxYGC <younggengchen@gmail.com>
2023-10-11 02:30:51 +0400

    init

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1696976477';
+const cacheName = 'chirpy-1696976983';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
